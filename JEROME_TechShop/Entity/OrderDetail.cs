3b751df7f0a74c184eed17a2765828f8184df2ec
JeromeAntoRezin20 <160305423+JeromeAntoRezin20@users.noreply.github.com>
2025-04-22 13:00:21 +0530

    Add files via upload

---
+++ JEROME_TechShop/Entity/OrderDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JEROME_TechShop.Entity
+{
+    public class OrderDetail
+    {
+        public int OrderDetailID { get; set; }
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ JEROME_TechShop/exception.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JEROME_TechShop.exception
+{
+    public class InvalidDataException : Exception
+    {
+        public InvalidDataException(string message) : base(message) { }
+    }
+
+    public class InsufficientStockException : Exception
+    {
+        public InsufficientStockException(string message) : base(message) { }
+    }
+}
